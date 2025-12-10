3b74da13a35c98923335648a0d54211c2dce9210
aifalabsglobal <kiran@aimtechnologies.in>
2025-12-10 14:46:47 +0530

    style: add navigation progress bar using nextjs-toploader

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "@/components/ui/toaster";
 import { ClerkProvider } from "@clerk/nextjs";
+import NextTopLoader from "nextjs-toploader";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -45,6 +46,7 @@ export default function RootLayout({
         <body
           className={`${geistSans.variable} ${geistMono.variable} antialiased bg-background text-foreground`}
         >
+          <NextTopLoader showSpinner={false} />
           {children}
           <Toaster />
         </body>
